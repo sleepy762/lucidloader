c0cb45a9688c4b34209efea67d8cfa725379fbbb
skydiver221 <binelemental@gmail.com>
2021-08-02 17:30:53 +0300

    Updated gitignore and made qemuScript more automatic

--- qemuScript.sh
+++ qemuScript.sh
@@ -1,9 +1,14 @@
+# Check if the directory exists and create it if it doesnt
+[ -d iso ] || mkdir iso
+
+# Create a FAT type image and add the EFI file into it
 dd if=/dev/zero of=fat.img bs=1k count=1440
 mformat -i fat.img -f 1440 ::
 mmd -i fat.img ::/EFI
 mmd -i fat.img ::/EFI/BOOT
 mcopy -i fat.img bootx64.efi ::/EFI/BOOT
 
+# Create an ISO image with out EFI file and start qemu to test it
 cp fat.img iso
 xorriso -as mkisofs -R -f -e fat.img -no-emul-boot -o cdimage.iso iso
 qemu-system-x86_64 -L ovmf/ -pflash ovmf/OVMF.fd -cdrom cdimage.iso
