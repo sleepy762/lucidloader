31988bf002556faf40be9efaf8f8d4dcc3c4ae81
haidush <binelemental@gmail.com>
2022-01-13 20:53:41 +0200

    Updated installer

--- installer.sh
+++ installer.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 # Installs the bootloader into the boot partition and adds a boot entry
 
-# Create the .efi binary of the bootloader
-echo "Compiling the bootloader..."
-make -j4 >/dev/null
-
 if [ "$EUID" -ne 0 ]; then
     echo "This script must be run as root."
     exit 1
 fi
 
-echo "Creating the directories and files..."
+# Move the bootloader into the created directory
 mkdir /boot/EFI/ezboot 2>/dev/null
-mv ezboot_x64.efi /boot/EFI/ezboot
+cp ezboot_x64.efi /boot/EFI/ezboot || exit 1
+rm ezboot_x64.efi
 touch /boot/EFI/ezboot/config.cfg
 
-echo "Getting boot partition info..."
 # Parses lsblk to get the boot disk + partition
 boot_disk="/dev/$(lsblk | grep boot | cut -c7- | cut -d " " -f 1)"
 boot_partition="${boot_disk: -1}"
 boot_disk="${boot_disk%?}"
 
-echo "Creating boot entry..."
-# Create the UEFI boot entry
+# Create the UEFI boot entry, if it doesn't exist already
 efibootmgr | grep ezboot >/dev/null || efibootmgr -c -d $boot_disk -p $boot_partition -L ezboot -l \\EFI\\ezboot\\ezboot_x64.efi >/dev/null
 
 echo "Installation complete! Make sure to configure the bootloader."
