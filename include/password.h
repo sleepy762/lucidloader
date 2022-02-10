8bc7c21a39bb654b6cda035c603a6bc1269ed058
Almog Amar <almoman33@gmail.com>
2022-02-10 17:23:21 +0200

    some more funcs

--- include/password.h
+++ include/password.h
@@ -3,5 +3,8 @@
 #include <uefi.h>
 
 #define PASS_FILE_PATH ("EFI\\ezboot\\shell_passwd")
+#define MAX_FILE_LEN 100
+#define ARR_LEN 16
 
 boolean_t CheackPassword(char_t password[]);
+void GetPass(char_t buffer[]); 
--- src/password.c
+++ src/password.c
@@ -2,5 +2,42 @@
 
 boolean_t CheackPassword(char_t password[])
 {
+    char_t buffer[MAX_FILE_LEN];
 
+    GetPass(buffer);
+
+    //enc conventions we set(just use the msg as key)
+    enc(password,password);
+    
+}
+
+void GetPass(char_t buffer[])
+{
+    FILE* file = fopen(PASS_FILE_PATH, "r");
+    if (file != NULL)
+    {
+        // Get file size
+        int64_t fileSize;
+        fseek(file, 0, SEEK_END);
+        fileSize = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        // Prevent a nasty bug from happening when calling fread() with size 0
+        if (fileSize == 0)
+        {
+            fclose(file);
+            return CMD_SUCCESS;
+        }
+
+        // Read the file data into a buffer
+        if (EFI_ERROR(status))
+        {
+            return CMD_OUT_OF_MEMORY;
+        }
+        fread(buffer, fileSize, 1, file);
+        buffer[fileSize] = CHAR_NULL;
+        fclose(file);
+
+        
+    }
 }
