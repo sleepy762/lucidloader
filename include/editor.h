1ab32ca016971c461b2b775ee1da26ff15c02637
haidush <binelemental@gmail.com>
2022-02-12 21:56:52 +0200

    Added some functions to the editor

--- include/editor.h
+++ include/editor.h
@@ -5,6 +5,10 @@
 #include "logger.h"
 #include "efiextendedinput.h"
 
-efi_key_data_t GetInputKeyData(efi_simple_text_input_ex_protocol_t* ConInEx);
+#define EDITOR_EXIT_KEY ('q')
+
+boolean_t IsKeyPressedWithLCtrl(efi_key_data_t keyData, char_t key);
 
 int8_t StartEditor(void);
+efi_key_data_t GetInputKeyData(efi_simple_text_input_ex_protocol_t* ConInEx);
+boolean_t ProcessEditorInput(efi_simple_text_input_ex_protocol_t* ConInEx);
--- src/editor.c
+++ src/editor.c
@@ -4,6 +4,8 @@ int8_t StartEditor(void)
 {
     ST->ConOut->ClearScreen(ST->ConOut);
 
+    // We need to get the extended text input protocol in order to get more data about key presses
+    // for example, to tell if the left control key was pressed with another key 
     efi_simple_text_input_ex_protocol_t* ConInEx;
     efi_guid_t extendedInputGuid = EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL_GUID;
     efi_status_t status = BS->LocateProtocol(&extendedInputGuid, NULL, (void**)&ConInEx);
@@ -13,22 +15,28 @@ int8_t StartEditor(void)
         return 1;
     }
 
-    while (1)
-    {
-        efi_key_data_t keyData = GetInputKeyData(ConInEx);
-        printf("char:%c scancode:%d\n", keyData.Key.UnicodeChar, keyData.Key.ScanCode);
-        printf("shift:%d toggle:%d\n\n", keyData.KeyState.KeyShiftState - EFI_SHIFT_STATE_VALID, keyData.KeyState.KeyToggleState - EFI_TOGGLE_STATE_VALID);
-
-        // close on ctrl+q
-        if (keyData.KeyState.KeyShiftState == EFI_SHIFT_STATE_VALID + EFI_LEFT_CONTROL_PRESSED && keyData.Key.UnicodeChar == 'q')
-        {
-            break;
-        }
-    }
+    // Keep reading and processing input until the editor is closed
+    while (ProcessEditorInput(ConInEx));
     
+    ST->ConOut->ClearScreen(ST->ConOut);
     return 0;
 }
 
+// Return FALSE when we want to stop processing input
+boolean_t ProcessEditorInput(efi_simple_text_input_ex_protocol_t* ConInEx)
+{
+    efi_key_data_t keyData = GetInputKeyData(ConInEx);
+    printf("scancode:%d char:%c", keyData.Key.ScanCode, keyData.Key.UnicodeChar);
+    printf("\n");
+    printf("shift:%d toggle:%d\n\n", keyData.KeyState.KeyShiftState - EFI_SHIFT_STATE_VALID, keyData.KeyState.KeyToggleState - EFI_TOGGLE_STATE_VALID);
+
+    // Close the editor
+    if (IsKeyPressedWithLCtrl(keyData, EDITOR_EXIT_KEY)) return FALSE;
+
+    return TRUE;
+}
+
+// This function is pretty much the same as GetInputKey but using the extended input protocol
 efi_key_data_t GetInputKeyData(efi_simple_text_input_ex_protocol_t* ConInEx)
 {
     uintn_t idx;
@@ -45,4 +53,10 @@ efi_key_data_t GetInputKeyData(efi_simple_text_input_ex_protocol_t* ConInEx)
     EnableWatchdogTimer(DEFAULT_WATCHDOG_TIMEOUT);
 
     return keyData;
+}
+
+boolean_t IsKeyPressedWithLCtrl(efi_key_data_t keyData, char_t key)
+{
+    return (keyData.KeyState.KeyShiftState == EFI_SHIFT_STATE_VALID + EFI_LEFT_CONTROL_PRESSED 
+        && keyData.Key.UnicodeChar == key);
 }
