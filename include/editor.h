b48d985fef7d7d44aa8d939a01c830430924d221
haidush <binelemental@gmail.com>
2022-02-13 22:52:58 +0200

    Added low level input processing

--- include/editor.h
+++ include/editor.h
@@ -7,8 +7,42 @@
 
 #define EDITOR_EXIT_KEY ('q')
 
+#define UP_ARROW_SCANCODE       (0x01)
+#define DOWN_ARROW_SCANCODE     (0x02)
+#define RIGHT_ARROW_SCANCODE    (0x03)
+#define LEFT_ARROW_SCANCODE     (0x04)
+
+#define PAGEUP_KEY_SCANCODE     (0x09)
+#define PAGEDOWN_KEY_SCANCODE   (0x0A)
+
+#define HOME_KEY_SCANCODE       (0x05)
+#define END_KEY_SCANCODE        (0x06)
+
+#define DELETE_KEY_SCANCODE     (0x08)
+
+typedef struct editor_config_t
+{
+    // Size of the screen in rows and columns
+    uintn_t screenRows;
+    uintn_t screenCols;
+
+    // The current cursor location
+    uintn_t cx;
+    uintn_t cy;
+} editor_config_t;
+
+typedef struct buffer_t
+{
+    char_t* b;
+    uint32_t len;
+} buffer_t;
+#define BUF_INIT {NULL, 0}
+
 boolean_t IsKeyPressedWithLCtrl(efi_key_data_t keyData, char_t key);
 
 int8_t StartEditor(void);
 efi_key_data_t GetInputKeyData(efi_simple_text_input_ex_protocol_t* ConInEx);
-boolean_t ProcessEditorInput(efi_simple_text_input_ex_protocol_t* ConInEx);
+
+void AppendToBuffer(buffer_t* buf, const char_t* str, uint32_t len);
+void PrintBuffer(buffer_t* buf);
+void FreeBuffer(buffer_t* buf);
--- src/editor.c
+++ src/editor.c
@@ -1,9 +1,109 @@
 #include "editor.h"
 
-int8_t StartEditor(void)
+static boolean_t ProcessEditorInput(efi_simple_text_input_ex_protocol_t* ConInEx);
+static editor_config_t cfg;
+
+/* Renderers */
+static void EditorDrawRows(buffer_t* buf)
 {
+    for (uint16_t y = 0; y < cfg.screenRows; y++)
+    {
+        // Temporary welcome message
+        if (y == cfg.screenRows / 3)
+        {
+            char welcome[80];
+            uintn_t welcomelen = snprintf(welcome, sizeof(welcome), "EZBoot Editor");
+            if (welcomelen > cfg.screenCols)
+            {
+                welcomelen = cfg.screenCols;
+            }
+            uintn_t padding = (cfg.screenCols - welcomelen) / 2;
+            if (padding)
+            {
+                AppendToBuffer(buf, "~", 1);
+                padding--;
+            }
+            while (padding--) AppendToBuffer(buf, " ", 1);
+            AppendToBuffer(buf, welcome, welcomelen);
+        }
+        else
+        {
+            AppendToBuffer(buf, "~", 1);
+        }
+
+        if (y < cfg.screenRows - 1)
+        {
+            AppendToBuffer(buf, "\n", 1);
+        }
+    }
+}
+
+static void EditorMoveCursor(uint16_t scancode)
+{
+    // Move the cursor while performing bounds checking
+    switch (scancode)
+    {
+        case UP_ARROW_SCANCODE:
+            if (cfg.cy != 0)
+            {
+                cfg.cy--;
+            }
+            break;
+        case DOWN_ARROW_SCANCODE:
+            if (cfg.cy != cfg.screenRows - 1)
+            {
+                cfg.cy++;
+            }
+            break;
+        case RIGHT_ARROW_SCANCODE:
+            if (cfg.cx != cfg.screenCols - 1)
+            {
+                cfg.cx++;
+            }
+            break;
+        case LEFT_ARROW_SCANCODE:
+            if (cfg.cx != 0)
+            {
+                cfg.cx--;
+            }
+            break;
+    }
+}
+
+static void EditorRefreshScreen(void)
+{
+    buffer_t buf = BUF_INIT;
+
+    ST->ConOut->EnableCursor(ST->ConOut, FALSE);
     ST->ConOut->ClearScreen(ST->ConOut);
 
+    EditorDrawRows(&buf);
+    PrintBuffer(&buf);
+
+    ST->ConOut->SetCursorPosition(ST->ConOut, cfg.cx, cfg.cy);
+    ST->ConOut->EnableCursor(ST->ConOut, TRUE);
+
+    FreeBuffer(&buf);
+}
+
+static void InitEditorConfig(void)
+{
+    // Query the amount of rows and columns
+    efi_status_t status = ST->ConOut->QueryMode(ST->ConOut, ST->ConOut->Mode->Mode, 
+        &cfg.screenCols, &cfg.screenRows);
+    if (EFI_ERROR(status))
+    {
+        Log(LL_ERROR, status, "Failed to get console width and height.");
+        // Use default values on error
+        cfg.screenCols = 80;
+        cfg.screenRows = 25;
+    }
+    cfg.cx = 0;
+    cfg.cy = 0;
+}
+
+int8_t StartEditor(void)
+{
     // We need to get the extended text input protocol in order to get more data about key presses
     // for example, to tell if the left control key was pressed with another key 
     efi_simple_text_input_ex_protocol_t* ConInEx;
@@ -15,6 +115,8 @@ int8_t StartEditor(void)
         return 1;
     }
 
+    InitEditorConfig();
+
     // Keep reading and processing input until the editor is closed
     while (ProcessEditorInput(ConInEx));
     
@@ -23,8 +125,10 @@ int8_t StartEditor(void)
 }
 
 // Return FALSE when we want to stop processing input
-boolean_t ProcessEditorInput(efi_simple_text_input_ex_protocol_t* ConInEx)
+static boolean_t ProcessEditorInput(efi_simple_text_input_ex_protocol_t* ConInEx)
 {
+    EditorRefreshScreen();
+
     efi_key_data_t keyData = GetInputKeyData(ConInEx);
     printf("scancode:%d char:%c", keyData.Key.ScanCode, keyData.Key.UnicodeChar);
     printf("\n");
@@ -32,6 +136,40 @@ boolean_t ProcessEditorInput(efi_simple_text_input_ex_protocol_t* ConInEx)
 
     // Close the editor
     if (IsKeyPressedWithLCtrl(keyData, EDITOR_EXIT_KEY)) return FALSE;
+
+    switch (keyData.Key.ScanCode)
+    {
+        // Move the cursor to the top or the bottom of the screen
+        case PAGEUP_KEY_SCANCODE:
+        case PAGEDOWN_KEY_SCANCODE:
+            uint32_t times = cfg.screenRows;
+            while (times--)
+            {
+                EditorMoveCursor(keyData.Key.ScanCode == PAGEUP_KEY_SCANCODE ? 
+                    UP_ARROW_SCANCODE : DOWN_ARROW_SCANCODE);
+            }
+            break;
+
+        // Move the cursor to the end or the beginning of the row
+        case HOME_KEY_SCANCODE:
+            cfg.cx = 0;
+            break;
+        case END_KEY_SCANCODE:
+            cfg.cx = cfg.screenCols - 1;
+            break;
+
+        case DELETE_KEY_SCANCODE:
+            // Nothing for now
+            break;
+
+        // Move the cursor one column/row
+        case UP_ARROW_SCANCODE:
+        case DOWN_ARROW_SCANCODE:
+        case RIGHT_ARROW_SCANCODE:
+        case LEFT_ARROW_SCANCODE:
+            EditorMoveCursor(keyData.Key.ScanCode);
+            break;
+    }
 
     return TRUE;
 }
@@ -59,4 +197,30 @@ boolean_t IsKeyPressedWithLCtrl(efi_key_data_t keyData, char_t key)
 {
     return (keyData.KeyState.KeyShiftState == EFI_SHIFT_STATE_VALID + EFI_LEFT_CONTROL_PRESSED 
         && keyData.Key.UnicodeChar == key);
+}
+
+void AppendToBuffer(buffer_t* buf, const char_t* str, uint32_t len)
+{
+    // Resize the string
+    char* new = realloc(buf->b, buf->len + len);
+
+    if (new == NULL)
+    {
+        return;
+    }
+    // Update the buffer struct with the new bigger string
+    memcpy(&new[buf->len], str, len);
+    buf->b = new;
+    buf->len += len;
+    buf->b[buf->len] = CHAR_NULL;
+}
+
+void FreeBuffer(buffer_t* buf)
+{
+    free(buf->b);
+}
+
+void PrintBuffer(buffer_t* buf)
+{
+    printf("%s", buf->b);
 }
