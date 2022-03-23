0fb0ba52958b4639877589ed85921de0af0f0e31
vladimir132 <binelemental@gmail.com>
2022-03-23 23:40:29 +0200

    Added changelog and fixed stack corruption

--- src/chainloader.c
+++ src/chainloader.c
@@ -19,7 +19,7 @@ void ChainloadImage(char_t* path, char_t* args)
     char_t* imgData = GetFileContent(path, &imgFileSize);
     if (imgData == NULL)
     {
-        Log(LL_ERROR, 0, "Failed to read file '%s' for chainloading: %s", path);
+        Log(LL_ERROR, 0, "Failed to read file '%s' for chainloading.", path);
         return;
     }
 
