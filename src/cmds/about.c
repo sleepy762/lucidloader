0ed6c358863d35112ebe4fd9585e37a614ef0140
vladimir132 <binelemental@gmail.com>
2022-04-01 16:56:15 +0300

    Merged printf calls

--- src/cmds/about.c
+++ src/cmds/about.c
@@ -6,8 +6,8 @@ boolean_t AboutCmd(cmd_args_s** args, char_t** currPathPtr)
 {   
     printf("%s v%s\nBuilt with POSIX-UEFI\n\n", EZBOOT_NAME_STR, EZBOOT_VERSION);
 
-    printf("Running on:\n");
-    printf("  Firmware: ");
+    printf("Running on:\n"
+           "  Firmware: ");
     ST->ConOut->OutputString(ST->ConOut, ST->FirmwareVendor);
     printf(" %d.%02d\n", ST->FirmwareRevision >> 16, ST->FirmwareRevision & ((1 << 16) - 1));
 
